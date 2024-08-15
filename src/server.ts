98e6dde9b7d4d4ce337439f7ba054966bd82315e
Albano <Albano Xhafaj>
2024-08-15 18:16:20 +0200

    feature: small refactoring

--- src/server.ts
+++ src/server.ts
@@ -7,20 +7,21 @@ import { config } from './config.js';
 import { connect } from './db/index.js';
 import { yoga } from './yoga/index.js'
 
-const app = express();
-
-app.use(yoga.graphqlEndpoint, yoga);
-
 (async () => {
   try {
     logger.setLevel(config.isProduction() ? 'INFO' : 'DEBUG');
     logger.debug('Started script');
 
+    const app = express();
+    app.use(yoga.graphqlEndpoint, yoga);
+
     logger.debug(`DB connecting: ${config.getDbUrl()}`);
 
     await connect(config.getDbUrl());
 
-    app.listen(config.getPort(), () => { logger.info(`Running a GraphQL API server at http://127.0.0.1:${config.getPort()}/graphql`); });
+    app.listen(config.getPort(), () => {
+      logger.info(`Running a GraphQL API server at http://127.0.0.1:${config.getPort()}/graphql`);
+     });
   } catch (err) {
     logger.error(err);
     logger.info('Script stopped unexpectedly');
